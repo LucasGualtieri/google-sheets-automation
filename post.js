8343feb72709de1c8eb4b824e77d612326cd39f8
Lucas Gualtieri <lucas.gualtieri@nexos.digital>
2026-03-16 21:19:20 -0300

    Refactor assertion handling in post.js by moving assert definitions to the top and removing duplicates. Added notes for Google Script compatibility.

--- post.js
+++ post.js
@@ -1,3 +1,9 @@
+// NOTE - Google Script não suporta console.assert(), mas pra testar é melhor usar!
+const USING_ASSERT = true;
+const assert = USING_ASSERT ? console.assert : custom_assert;
+// NOTE - Set pra true se quiser parar no primeiro erro quando testando direto no Google Script
+const ASSERT_ERROR_THROWS_EXCEPTION = false;
+
 const SHEET_NAME = "Gastos de 17/02 à 17/03 Novo";
 
 // TODO - Consertar essa função
@@ -48,12 +54,6 @@ const HANDLERS = [
 	NuEstorno,
 	CompraNuCreditoDebito,
 ];
-
-// NOTE - Google Script não suporta console.assert(), mas pra testar é melhor usar!
-const USING_ASSERT = true;
-const assert = USING_ASSERT ? console.assert : custom_assert;
-// NOTE - Set pra true se quiser parar no primeiro erro quando testando direto no Google Script
-const ASSERT_ERROR_THROWS_EXCEPTION = false;
 
 // ─── Entry Point ─────────────────────────────────────────────────────────────
 
